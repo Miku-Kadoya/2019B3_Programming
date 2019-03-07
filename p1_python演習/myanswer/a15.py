b3f47b41b06ed76dbd5143461213b17122cfd0ef
miku-kadoya <mikuuu1515gon@gmail.com>
2019-03-07 13:29:30 +0900

    15問目の解答を追加

--- p1_python演習/myanswer/a15.py
+++ p1_python演習/myanswer/a15.py
@@ -3,8 +3,6 @@ import numpy as np
 docs = [["リンゴ", "リンゴ"], ["リンゴ", "レモン"], ["レモン", "ミカン"]]
 terms = ["リンゴ", "レモン", "ミカン"]
 
-a = np.zeros((len(docs), len(terms)))
-
 def tf(term, doc):
     count = 0
     for word in doc:
@@ -19,8 +17,11 @@ def idf(term, docs):
             count += 1
     return np.log10(len(docs) / count) + 1
 
-for i, doc in enumerate(docs):
-    for j, term in enumerate(terms):
-        a[i][j] = tf(term, doc) * idf(term, docs)
+def tf_idf(docs, terms):
+    a = np.zeros((len(docs), len(terms)))
+    for i, doc in enumerate(docs):
+      for j, term in enumerate(terms):
+          a[i][j] = tf(term, doc) * idf(term, docs)
+    return a
 
-print(a)
+print(tf_idf(docs, terms))
