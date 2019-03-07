00009926cfeae9dce8d8695401a3acbc8702b36e
角谷未来 <kadoyamiku@kadoyamirai-no-MacBook-Pro.local>
2019-03-07 09:45:26 +0900

    10問目の解答を追加

---
+++ p1_python演習/myanswer/a10.py
@@ -0,0 +1,11 @@
+def ngram(str,n):
+    ngram = []
+    for i in range(len(str) - n + 1):
+        ngram.append(str[i:i+n])
+    return ngram
+
+str = input()
+word = str.split(' ')
+char = "".join(word)
+print('単語bi-gram: ' , ngram(word,2))
+print('文字bi-gram: ' , ngram(char,2))
