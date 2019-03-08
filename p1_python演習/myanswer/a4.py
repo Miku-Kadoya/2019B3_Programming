0513b551a651aa12cae37597628ea11ec2abaf42
miku-kadoya <mikuuu1515gon@gmail.com>
2019-03-08 11:48:35 +0900

    4問目の解答を変更しました

--- p1_python演習/myanswer/a4.py
+++ p1_python演習/myanswer/a4.py
@@ -3,9 +3,9 @@ str1 = ''
 str2 =''
 for i in range(len(str)):
     if (i % 2 == 0):
-        str1 += str[i:(i+1)]
+        str1 += str[i]
     else:
-        str2 += str[i:(i+1)]
+        str2 += str[i]
 
 print(str1)
 print(str2)
