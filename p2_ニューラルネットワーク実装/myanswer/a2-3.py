da53b7dc1310898f7c1d4238cf9769fb005bf211
miku-kadoya <mikuuu1515gon@gmail.com>
2019-03-11 11:35:02 +0900

    演習2-3の回答を変更

--- p2_ニューラルネットワーク実装/myanswer/a2-3.py
+++ p2_ニューラルネットワーク実装/myanswer/a2-3.py
@@ -4,16 +4,15 @@ def relu(x):
     return np.maximum(0, x)
 
 class SingleLayer:
-    def __init__(self, x, W, b):
-        self.x = x
+    def __init__(self, W, b):
         self.W = W
         self.b = b
 
-    def forward(self):
-        return relu(np.dot(self.W.T, self.x) + self.b)
+    def forward(self, x):
+        return relu(np.dot(self.W.T, x) + self.b)
 
 x = np.array([1.0, 0.5])
 W = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
 b = np.array([0.1, 0.2, 0.3])
-l = SingleLayer(x, W, b)
-print(l.forward())
+l = SingleLayer(W, b)
+print(l.forward(x))
