40533b5c03f8a71f965f9593b0100d7fe1f99a0d
miku-kadoya <mikuuu1515gon@gmail.com>
2019-03-11 15:17:36 +0900

    演習3-4の回答を変更

--- p2_ニューラルネットワーク実装/myanswer/a3-4.py
+++ p2_ニューラルネットワーク実装/myanswer/a3-4.py
@@ -21,12 +21,17 @@ class Affine():
         self.dx = np.dot(dz, self.W.T)
         return self.dW, self.db, self.dx
 
-x = np.array([[1.0, 0.5], [-0.4, 0.1]])
-W = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-b = np.array([0.1, 0.2, 0.3])
-aff = Affine()
-print("順伝播出力: \n", aff.forward(x, W, b))
-dW, db, dx = aff.backprop(np.ones_like(W))
-print("逆伝播出力dx: \n", dx)
-print("逆伝播出力dW: \n", dW)
-print("逆伝播出力db: \n", db)
+
+def main():
+    x = np.array([[1.0, 0.5], [-0.4, 0.1]])
+    W = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    b = np.array([0.1, 0.2, 0.3])
+    aff = Affine()
+    print("順伝播出力: \n", aff.forward(x, W, b))
+    dW, db, dx = aff.backprop(np.ones_like(W))
+    print("逆伝播出力dx: \n", dx)
+    print("逆伝播出力dW: \n", dW)
+    print("逆伝播出力db: \n", db)
+
+if __name__=="__main__":
+    main()
